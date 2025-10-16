4a5b17db25fbeb27b3b4099c9333e4480cf446bb
Ashley Domineck <adouglas0721@gmail.com>
2025-10-15 22:03:47 -0400

    Post Schema

---
+++ models/postSchema.mjs
@@ -0,0 +1,45 @@
+
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    authorID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, 
+    },
+
+    chapterID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chapter",
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 1000,
+    },
+
+    // picture: {
+    //   type: String, 
+    //   trim: true,
+    //   default: "",
+    // },
+
+    likeCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+// I would love to add comments
+
+PostSchema.index({ createdAt: -1 });
+PostSchema.index({ chapterID: 1, createdAt: -1 });
+
+export default mongoose.model("Post", PostSchema);
